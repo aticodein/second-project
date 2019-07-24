999513c6ad01c5ddfb7f768d512249f06b56e5ff
Ubuntu <ubuntu@ip-172-31-43-107.ec2.internal>
2019-07-24 10:04:49 +0000

    cancel navbar

--- main/js/reset-toggle.js
+++ main/js/reset-toggle.js
@@ -5,20 +5,3 @@ $('#reset-filters').click(function() {
     dc.renderAll();
 });
 
-/* ------------------------------------------------ Toggle -*/
-
-$('.hide-content-btn').click(function() {
-    $(this).parent().nextAll('.hide-content-toggle').slideToggle();
-    $(this).parent().parent().parent().toggleClass('eq-column-height');
-    $(this).toggleClass('fa-angle-double-up');
-    $(this).toggleClass('fa-angle-double-down');
-});
-
-//Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-//function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
