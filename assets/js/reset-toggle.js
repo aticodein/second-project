52a016c291255708461bad5ced473f42e296efbc
Ubuntu <ubuntu@ip-172-31-43-107.ec2.internal>
2019-08-02 13:00:20 +0000

    js comments, thank you js footer icon

--- assets/js/reset-toggle.js
+++ assets/js/reset-toggle.js
@@ -1,22 +1,35 @@
-/* ------------------------------------------------ Reset -*/
+/* ------------------------------------------------ Reset button -----------*/
 
 $('#reset-filters').click(function(resetAll) {
     dc.filterAll();
     dc.renderAll();
 });
-
+/* ------------------------------------------------ Toggle effect for icons -----------*/
 $('.hide-content-btn').click(function(hideContent) {
     $(this).parent().nextAll('.hide-content-toggle').slideToggle(500);
     $(this).parent().parent().toggleClass('toggle');
     $(this).toggleClass('fa-book');
     $(this).toggleClass('fa-book-open');
 });
-
+/* ------------------------------------------------ Learn more button popup -----------*/
 function learnMore() {
   var x = document.getElementById("buttonSuccess");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
+  if (x.style.display === "none") {
     x.style.display = "block";
+  } else {
+    x.style.display = "none";
   }
 }
+
+learnMore();
+/* ------------------------------------------------ JS footer icon thank you popup -----------*/
+function thankYou() {
+  var x = document.getElementById("javascript");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+thankYou();
